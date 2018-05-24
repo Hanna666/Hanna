a88475c673a0c432c438ef757d1f4731995db3c8
Hanna666 <firiat66613@gmail.com>
2018-05-24 16:41:20 +0300

    Создан класс Point и функция вычисления расстояния между точками

--- sandbox/src/main/java/Hanna/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/Hanna/sandbox/MyFirstProgram.java
@@ -4,6 +4,15 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+   Point a = new Point(2, 4);
+   Point b = new Point(7, 11);
+
+    System.out.println("Расстояние между двумя точками на плоскости с координатами точки a="+ a.p1+";"+a.p2 + " и точки b="+b.p1+";"+b.p2+" равно "+ distance(a,b));
+
+  }
+  public static double distance(Point a, Point b) {
+    return Math.sqrt((a.p2 - a.p1) * (a.p2 - a.p1) + (b.p2 - b.p1) * (b.p2 - b.p1));
   }
 
-}	
+}
