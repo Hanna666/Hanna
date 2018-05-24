8e65e58ee38679062bb39fdf6add7cbbd43559ab
Hanna666 <firiat66613@gmail.com>
2018-05-24 20:14:45 +0300

    Реализовано вычисления расстояния между точками через метод в классе Point

--- sandbox/src/main/java/Hanna/sandbox/Point.java
+++ sandbox/src/main/java/Hanna/sandbox/Point.java
@@ -4,11 +4,18 @@ public class Point {
 
     public double p1;
     public double p2;
+
+
     public Point (double p1, double p2) {
         this.p1 = p1;
         this.p2 = p2;
+
     }
 
+    public double distance(Point second) {
+        // Math.sqrt((a.p2 - a.p1) * (a.p2 - a.p1) + (b.p2 - b.p1) * (b.p2 - b.p1));
+        return Math.sqrt((this.p2 - this.p1)*(this.p2 - this.p1)+(second.p2 - second.p1)*(second.p2 - second.p1));
+    }
 
     }
 
