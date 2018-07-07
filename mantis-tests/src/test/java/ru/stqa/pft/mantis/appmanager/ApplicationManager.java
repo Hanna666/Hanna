7adcb53fffea4002fb98ef6ddc8f764f76d38040
Hanna666 <firiat66613@gmail.com>
2018-07-07 17:58:47 +0300

    Реализована интеграция тестов с Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
     private FtpHelper ftp;
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
+    private SoapHelper soapHelper;
 
 
     public ApplicationManager(String browser) {
@@ -91,5 +92,10 @@ public class ApplicationManager {
         return jamesHelper;
     }
 
-
+    public SoapHelper soap() {
+        if (soapHelper == null) {
+            soapHelper = new SoapHelper(this)
+        }
+        return soapHelper;
+    }
 }
